fdac16182de37b87a382541286ac28bef7f6994f
Elise <crimsoncrescentmoon@gmail.com>
2016-05-04 01:20:04 -0500

    if moved previously

--- BUDDY_ALLOCATOR/proj3/buddy.c
+++ BUDDY_ALLOCATOR/proj3/buddy.c
@@ -171,6 +171,7 @@ void *buddy_alloc(int size)
 void buddy_free(void *addr)
 {
 	/* TODO: IMPLEMENT THIS FUNCTION */
+	int move = 0;
 	page_t * pg = list_entry( addr, page_t, list );
 
 	while( pg->order < MAX_ORDER )
@@ -196,12 +197,20 @@ void buddy_free(void *addr)
 				{
 					list_move( &g_pages[buddy_index].list, &free_area[pg->order] );
 				}
+				move = 1;
 				break;
 			}
 		}
 		if( !found )
 		{
-			list_add( &g_pages[pg->index].list, &free_area[pg->order] );
+			if( move )
+			{
+				list_move( &g_pages[pg->index].list, &free_area[pg->order]);
+			}
+			else
+			{
+				list_add( &g_pages[pg->index].list, &free_area[pg->order] );
+			}
 			break;
 		}
 
